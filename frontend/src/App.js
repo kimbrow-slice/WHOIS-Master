ab9873e11e22b86fe258ee8f19ca012992c2cfa7
Jeffery Kimbrow <71730781+kimbrow-slice@users.noreply.github.com>
2021-03-13 18:02:08 -0600

    created form.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,58 +4,75 @@ import axios from 'axios'
 import './App.css';
 import List from './components/list';
 import withListLoading from './components/withListLoading';
+import Form from './form.js';
 
-function App() {
-  
 
-  const ListLoading = withListLoading(List);
-  const [appState, setAppState] = useState({
-    loading: false,
-    repos: null,
-  
-  });
+class App extends Component {
+  state = {
+    fields: ""
+  };
 
-  useEffect(() => {
-    setAppState({ loading: true });
-    const apiUrl = 'http://localhost:4000/api/whois/';
-    axios.get(apiUrl).then((lookupData) => {
-      console.log("hi",lookupData.data)
-      const ipDomain = lookupData.data;
-      setAppState({ loading: false, data: ipDomain });
-    });
-  }, [setAppState]);
 
-  
-  return (
-    <div className='App'>
-      <div className='container'>
-        <h1>WHOIS Lookup</h1>
-      </div>
-      <div className='whois-container'>
-        {/* Create a form that allows the user to input either a domain or ip address */}
-        <form>
-        <ListLoading isLoading={appState.loading} whois={appState.data} />
-        <br />
-        <label for="inputeValue">Enter an IP Address  or Domain Name Below</label>
-        <br />
-        <input id="inputValue" type="text" placeholder="IP Address or Domain" name="value" />
-        <br />
-        {/* Create a button that will trigger the API to display the data from what was input above */}
-       <p><button className="lookupBtn">Lookup some Data</button></p> 
-        <br />
-        </form>
-        <div className="results">
-          {/* Display the results of the search here */}
+  onSubmit = fields => {
+    console.log('App component got:', fields)
+    this.setState({ fields });
+  };
+
+  render() {
+    return(
+      <div className="App">
+        <div className="container">
+          <h1>WHOIS Lookup Application</h1>
         </div>
+        {/* populate the page with form.js */}
+        <Form onSubmit={fields => this.onSubmit(fields)} />
+        <p>{JSON.stringify(this.state.fields, null, 2)}</p>
+        <footer>
+          <div className='footer'>
+           Built with React and Node
+           by Jeffery Kimbrow
+         </div>
+       </footer>
       </div>
-
-      <footer>
-        <div className='footer'>
-          Built with React and Node
-          by Jeffery Kimbrow
-        </div>
-      </footer>
-    </div>
-  );
+    );
+  }
 }
 export default App;
+// function App() {
+  
+
+//   const ListLoading = withListLoading(List);
+//   const [appState, setAppState] = useState({
+//     loading: false,
+//     lookupData: null,
+  
+//   });
+
+//   useEffect(() => {
+//     setAppState({ loading: true });
+//     const apiUrl = 'http://localhost:4000/api/whois/';
+//     axios.get(apiUrl).then((lookupData) => {
+//       console.log("hi",lookupData.data)
+//       const ipDomain = lookupData.data;
+//       setAppState({ loading: false, data: ipDomain });
+//     });
+//   }, [setAppState]);
+
+
+
+//   return (
+//     <div className='App'>
+//       <div className='container'>
+//         <h1>WHOIS Lookup</h1>
+//       </div>
+//       <div className='whois-container'>
+//         {/* Create a form that allows the user to input either a domain or ip address */}
+//        <Form onSubmit={fields =>this.formSubmit(fields)} />
+
+//       </div>
+
+//      
+//     </div>
+//   );
+// }
+
---
+++ frontend/src/form.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class Form extends React.Component {
+    state = {
+        inputValue: ''
+    }
+    change = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        this.props.onSubmit(this.state)
+        console.log(this.state);
+        this.setState({
+            inputValue: ''
+        });
+    };
+
+    render() {
+        return(
+            /* Create a form that allows the user to input either a domain or ip address */
+            <form>
+                <input name="inputValue" placeholder="IP Address or Domain" value={this.state.inputValue} 
+                onChange={ e => this.change(e) }/>
+                <br />
+                <button onClick={e => this.onSubmit(e)}>Submit</button>
+            </form>
+        );
+    }
+
+}
